67f06f1247ba9810fbea3bfb9f525d84a459ee78
jomariepolanco <jomariepolanco@gmail.com>
2020-10-10 11:20:10 -0400

    done

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,18 @@
 def oxford_comma(array)
-
+    if array.length == 1
+        array.join("")
+    elsif array.length == 2
+        array.insert(1, 'and').join(" ")
+    elsif array.length == 3 
+        array[0] << ","
+        array[1] << "," 
+        array.insert(2, 'and').join(" ")
+    elsif array.length > 3
+        array.map do |ele| 
+            if ele != array.last
+                ele << ","
+            end 
+        end 
+        array.insert(array.length - 1, 'and').join(" ")
+    end
 end
